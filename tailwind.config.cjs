8278ff7164b3a6fc6fdf96062014630d0d76c0ec
Abdallah Samy <abdallhsamy2011@gmail.com>
2022-09-12 13:39:51 +0200

    home page template

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rose: colors.rose,
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
