aa551f07b7a0a3d538ba8d3a798751ef6c32621e
Abdallah Samy <abdallhsamy2011@gmail.com>
2022-09-12 14:12:02 +0200

    install all built-in tailwind plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,9 @@ module.exports = {
     },
   },
   plugins: [
+    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
   ],
 }
